d4826720dc737ec71874ee59b2a6f30a205afca5
JackieCisneros <krieger.jacqueline@gmail.com>
2023-03-15 10:48:57 -0600

    good all done

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,12 @@
 
 class Person:
     # Class body goes here
+    def walk(self):
+        print("Hello World!")
+    def talk(self):
+        print("The person is walking.")
 
+jack= Person()
+jack.walk()
+jack.talk()
     #Instance method definition
-    pass
